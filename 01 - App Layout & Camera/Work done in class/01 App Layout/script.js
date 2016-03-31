1fed71ad1ec56a45bcf017983c2891c7c67482d5
Phil Reither <philipp.reither@gmail.com>
2016-03-31 15:23:51 +0200

    Coding

--- 01 - App Layout & Camera/Work done in class/01 App Layout/script.js
+++ 01 - App Layout & Camera/Work done in class/01 App Layout/script.js
@@ -1,14 +1,21 @@
 
+var navElement = document.querySelector('nav');
+var closeIcon = document.querySelector('.fa-times');
+var openIcon = document.querySelector('.fa-bars');
+
 // when user clicks on menu icon
 // remove class "hide" from nav
-
-document.querySelector('.fa-times').onclick = hideMenu;
+closeIcon.onclick = hideMenu;
 
 function hideMenu() {
-    document.querySelector('nav').classList.add('hide');
+    navElement.classList.add('hide');
 }
 
 
 // when user clicks on close icon
 // add class "hide" to nav
+openIcon.onclick = showMenu;
 
+function showMenu() {
+    navElement.classList.remove('hide');
+}
