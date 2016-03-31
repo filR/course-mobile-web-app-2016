1a4615ffd16e3fd6c15d69e043e931febbd8282f
Phil Reither <philipp.reither@gmail.com>
2016-03-31 09:39:03 +0200

    Move files around

--- 01 - App Layout & Camera/Solution/01 App Layout/script.js
+++ 01 - App Layout & Camera/Solution/01 App Layout/script.js
@@ -1,16 +1,20 @@
 'use strict';
 
+// get all elements we are going to use
 let menu = document.querySelector('.menu');
 let openLink = document.querySelector('[action=open-menu]');
 let closeLink = document.querySelector('[action=close-menu]');
 
+// opens the menu by applying a css class
 function openMenu() {
     menu.classList.add('show');
 }
 
+// removes the class
 function closeMenu() {
     menu.classList.remove('show');
 }
 
+// open and close menu when clickng on the icons
 openLink.onclick = openMenu;
 closeLink.onclick = closeMenu;
--- 01 - App Layout & Camera/Solution/03 Draw/script.js
+++ 01 - App Layout & Camera/Solution/03 Draw/script.js
@@ -1,35 +1,45 @@
 'use strict';
 
+// our default draw values
 let brush_size = 30;
 let currentColor = '#E3EB64';
 
+// get our canvas and initialiase it
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
+// set size to full page size
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 
 
 
 // STEP 1: draw circles on move
-
+//
 //function draw (e) {
+//    
+//    // get the position of our mouse
 //    let posX = e.pageX;
 //    let posY = e.pageY;
 //    
+//    // get the position of our touch
 //    if (e.touches) {
 //        posX = e.touches[0].pageX;
 //        posY = e.touches[0].pageY;
 //    }
 //
+//    // step 1: draw a rectangle at that position
 ////    ctx.fillRect(posX-5, posY-5, 10, 10);
 //
+//    // step 2: draw a circle at that position
 //    ctx.beginPath();
-//    ctx.arc(posX, posY, BRUSH_SIZE, 0, Math.PI * 2, true);
+//    ctx.arc(posX, posY, brush_size, 0, Math.PI * 2, true);
 //    ctx.closePath();
+//    ctx.fillStyle = currentColor;
 //    ctx.fill();
 //}
 //
+//// whenever the mouse (or touch) moves, call the `draw` function
 //canvas.addEventListener('mousemove', draw);
 //canvas.addEventListener('touchmove', draw);
 
@@ -40,6 +50,7 @@ canvas.height = document.body.clientHeight;
 canvas.addEventListener('touchstart', startLine);
 canvas.addEventListener('touchmove', drawLine);
 
+// begin the line on a new touch
 function startLine(e) {
 	ctx.beginPath();
     ctx.lineWidth = brush_size;
@@ -48,6 +59,7 @@ function startLine(e) {
 	ctx.moveTo(e.touches[0].pageX, e.touches[0].pageY);
 }
 
+// update line whenever the touch moves
 function drawLine(e) {
     ctx.lineTo(e.touches[0].pageX, e.touches[0].pageY);
     ctx.stroke();
@@ -57,12 +69,15 @@ function drawLine(e) {
 
 // STEP 3: set colour
 
+// when the user touches the settings icon, call the function `toggleOverlay`
 document.querySelector('[action=toggle-settings]').onclick = toggleOverlay;
 
 function toggleOverlay() {
     $('.settings').toggle();
 }
 
+// when the user clicks on a `change-color` link, call the function `changeColor`
+// in the lines above, we used vanilla JavaScript, here we use jQuery
 $(document).on('click', '[action=change-color]', changeColor);
 
 function changeColor() {
