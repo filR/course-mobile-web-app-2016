6475598d0894d88320948c61a0a03dbeb9743c9a
Phil Reither <philipp.reither@gmail.com>
2016-03-31 16:18:29 +0200

    coding

--- 01 - App Layout & Camera/Work done in class/02 Camera/script.js
+++ 01 - App Layout & Camera/Work done in class/02 Camera/script.js
@@ -9,3 +9,44 @@ function previewImage() {
     
     reader.readAsDataURL(this.files[0]);
 }
+
+
+
+
+
+$('input[type=range]').change(applyEffect);
+
+function applyEffect() {
+    var value = $(this).val();
+    var cssValue = 'hue-rotate(' + value + 'deg)';
+    
+    $('#preview').css('-webkit-filter', cssValue);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
