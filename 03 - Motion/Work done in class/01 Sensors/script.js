e8f4f14768c54fff26331dfaf03ccc5f527a0749
Phil Reither <philipp.reither@gmail.com>
2016-04-08 14:06:42 +0200

    Add code from class

--- 03 - Motion/Work done in class/01 Sensors/script.js
+++ 03 - Motion/Work done in class/01 Sensors/script.js
@@ -1,11 +1,41 @@
 function handleMotionEvent(event) {
+    var x = event.acceleration.x || event.accelerationIncludingGravity.x;;
+    var y = event.acceleration.y || event.accelerationIncludingGravity.y;;
+    var z = event.acceleration.z || event.accelerationIncludingGravity.z;;
 
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
-
-    // Do something awesome.
+    // optional
+    x = Math.round(x * 100) / 100;
+    y = Math.round(y * 100) / 100;
+    z = Math.round(z * 100) / 100;
+    
+    document.querySelector('#x').innerText = x;
+    document.querySelector('#y').innerText = y;
+    document.querySelector('#z').innerText = z;
+    
     console.log(x, y, z);
+    
+    var max = Math.max(x, y, z); // highest acceleration
+    
+    max *= 20;
+    max = Math.abs(max); // always positive
+    max = Math.min(max, 100); // not more than 100
+    max = Math.round(max);
+    
+    var color = 'hsl(0, ' + max + '%, 50%)';
+    document.querySelector('body').style.background = color;
 }
 
 window.addEventListener("devicemotion", handleMotionEvent, true);
+
+
+
+
+
+
+
+
+
+
+
+
+
