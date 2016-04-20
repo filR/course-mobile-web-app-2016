2b5757df080065a90be9550f98102ea55e24ed5c
Phil Reither <philipp.reither@gmail.com>
2016-04-20 14:42:57 +0200

    Add code from class

--- 05 - Chat using a Backend (Firebase)/Work done in class/script.js
+++ 05 - Chat using a Backend (Firebase)/Work done in class/script.js
@@ -1,13 +1,14 @@
 
+// create new firebase app
 var app = new Firebase('https://voll-coole-chatapp.firebaseio.com');
 
-
+// write data to server
 app.push({
     name: 'Trump',
     text: 'MAKING THE WEB COOL AGAIN'
 });
 
-
+// get last 20 and listen to updates
 app.limitToLast(20).on('child_added', function (data) {
-   console.log(data.val());    
+   data.val();
 });
