b6e48fe097d4e474f9890f14e68b3e3eba0e4b8f
Phil Reither <philipp.reither@gmail.com>
2016-04-20 15:01:53 +0200

    Add code from class

--- 05 - Chat using a Backend (Firebase)/Work done in class/script.js
+++ 05 - Chat using a Backend (Firebase)/Work done in class/script.js
@@ -10,15 +10,13 @@ app.push({
 
 // get last 20 and listen to updates
 app.limitToLast(20).on('child_added', function (data) {
-   
-//        <li>
-//            <strong>Supraman:</strong>
-//            Your breathe is minty.
-//        </li>
+    // this function is called once for every message
     
+    // grab data from message
     var name = data.val().name;
     var text = data.val().text;
     
+    // normalise data
     if (!name) {
         name = "Anonymous";
     }
@@ -27,10 +25,15 @@ app.limitToLast(20).on('child_added', function (data) {
         return;
     }
     
+//    <li>
+//        <strong>Supraman:</strong>
+//        Your breathe is minty.
+//    </li>
+    
+    // create html element
     var element = $('<li><strong>' + name +
                     '</strong>: ' + text + '</li>');
     
+    // append to our container
     $("#messages").append(element);
-    
-    
 });
